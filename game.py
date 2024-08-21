c01f8d5032797b32f57b1b05bb2156a09651cde3
v52665lm <lewis.murphy@student.manchester.ac.uk>
2024-08-21 14:28:13 +0100

    added grid to show cells

--- game.py
+++ game.py
@@ -5,15 +5,25 @@ import pygame
 WIDTH = 1000
 HEIGHT = 1000
 RESOLUTION = 10
+CELLS_X = WIDTH // 2
+CELLS_Y = HEIGHT // 2
 
 # Create grid to store game
-grid = np.zeros((HEIGHT // RESOLUTION, WIDTH // RESOLUTION))
+grid = np.zeros((CELLS_Y, CELLS_Y))
 
 # Perform initialisation for pygame
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Conway's Game of Life")
 clock = pygame.time.Clock()
 running = True
+
+screen.fill("black")
+for i in range(0, CELLS_Y):
+    y = i * RESOLUTION
+    for j in range(0, CELLS_X):
+        x = j * RESOLUTION
+        pygame.draw.rect(screen, (0, 0, 0), (x, y, RESOLUTION, RESOLUTION), 1)
 
 # Basic game loop
 while running:
@@ -21,8 +31,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    screen.fill("purple")
 
     pygame.display.flip()
 
